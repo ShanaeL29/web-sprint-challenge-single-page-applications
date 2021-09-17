3751a1d9ad1518a4ab1a84befe6e66784d25ad9a
Shanae <sleslie18@gmail.com>
2021-09-17 12:08:11 -0400

    App.js and PizzaForm.js complete with error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,163 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./Homepage";
+import PizzaForm from "./PizzaForm";
+// import Orders from "./Orders";
+import Order from "./Order";
+import schema from "./validation/pizzaFormSchema";
+import axios from "axios";
+import * as yup from "yup";
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
+const initialPizzaFormValues = {
+  name: "",
+  size: "",
+  sauce: "",
+  pepperoni: false,
+  sausage: false,
+  canadianBacon: false,
+  spicyItalianSausage: false,
+  grilledChicken: false,
+  mushrooms: false,
+  bananaPeppers: false,
+  onions: false,
+  greenPeppers: false,
+  dicedTomatoes: false,
+  blackOlives: false,
+  roastedGarlic: false,
+  artichokeHearts: false,
+  threeCheese: false,
+  pineapple: false,
+  extraCheese: false,
+  glutenFree: false,
+  special: "",
+  quantity: "",
 };
-export default App;
+
+const initialPizzaFormErrors = {
+  name: "",
+  size: "",
+  sauce: "",
+  special: "",
+  quantity: "",
+};
+
+const initialState = [];
+const initialDisabled = true;
+
+export default function App() {
+  const [orders, setOrders] = useState(initialState);
+
+  const [pizzaFormValues, setPizzaFormValues] = useState(
+    initialPizzaFormValues
+  );
+  const [pizzaFormErrors, setPizzaFormErrors] = useState(
+    initialPizzaFormErrors
+  );
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => setPizzaFormErrors({ ...pizzaFormErrors, [name]: "" }))
+      .catch((err) =>
+        setPizzaFormErrors({ ...pizzaFormErrors, [name]: err.errors[0] })
+      );
+  };
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setPizzaFormValues({ ...pizzaFormValues, [name]: value });
+  };
+
+  const postNewOrder = (testOrder) => {
+    axios
+      .post("https://reqres.in/api/orders", testOrder)
+      .then((res) => {
+        console.log(res);
+        setOrders([res.data, ...orders]);
+      })
+      .catch((err) => console.error(err));
+    setPizzaFormValues(initialPizzaFormValues);
+  };
+
+  const formSubmit = () => {
+    const testOrder = {
+      // Initial method but did not list toppings as booleans...
+      //   name: pizzaFormValues.name.trim(),
+      //   size: pizzaFormValues.size,
+      //   sauce: pizzaFormValues.sauce.trim(),
+      //   toppings: [
+      //     "pepperoni",
+      //     "sausage",
+      //     "canadianBacon",
+      //     "spicyItalianSausage",
+      //     "grilledChicken",
+      //     "mushrooms",
+      //     "bananaPeppers",
+      //     "onions",
+      //     "greenPeppers",
+      //     "dicedTomatoes",
+      //     "blackOlives",
+      //     "roastedGarlic",
+      //     "artichokeHearts",
+      //     "threeCheese",
+      //     "pineapple",
+      //     "extraCheese",
+      //   ].filter((topping) => !!pizzaFormValues[topping]),
+      //   glutenFree: pizzaFormValues.glutenFree,
+      //   special: pizzaFormValues.special,
+      //   quantity: pizzaFormValues.quantity,
+      // };
+      name: pizzaFormValues.name.trim(),
+      size: pizzaFormValues.size,
+      sauce: pizzaFormValues.sauce.trim(),
+      pepperoni: pizzaFormValues.pepperoni,
+      sausage: pizzaFormValues.sausage,
+      canadianBacon: pizzaFormValues.canadianBacon,
+      spicyItalianSausage: pizzaFormValues.spicyItalianSausage,
+      grilledChicken: pizzaFormValues.grilledChicken,
+      mushrooms: pizzaFormValues.mushrooms,
+      bananaPeppers: pizzaFormValues.bananaPeppers,
+      onions: pizzaFormValues.onions,
+      greenPeppers: pizzaFormValues.greenPeppers,
+      dicedTomatoes: pizzaFormValues.dicedTomatoes,
+      blackOlives: pizzaFormValues.blackOlives,
+      roastedGarlic: pizzaFormValues.roastedGarlic,
+      artichokeHearts: pizzaFormValues.artichokeHearts,
+      threeCheese: pizzaFormValues.threeCheese,
+      pineapple: pizzaFormValues.pineapple,
+      extraCheese: pizzaFormValues.extraCheese,
+      glutenFree: pizzaFormValues.glutenFree,
+      special: pizzaFormValues.special,
+      quantity: pizzaFormValues.quantity,
+    };
+    postNewOrder(testOrder);
+  };
+
+  useEffect(() => {
+    schema.isValid(pizzaFormValues).then((valid) => setDisabled(!valid));
+  }, [pizzaFormValues]);
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/pizza">
+          <PizzaForm
+            values={pizzaFormValues}
+            change={inputChange}
+            submit={formSubmit}
+            disabled={disabled}
+            errors={pizzaFormErrors}
+          />
+        </Route>
+        {orders.map((order) => {
+          return <Order key={order.name} details={order} />;
+        })}
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
---
+++ src/PizzaForm.js
@@ -0,0 +1,285 @@
+import React from "react";
+
+export default function PizzaForm(props) {
+  const { values, submit, change, disabled, errors } = props;
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    submit();
+  };
+
+  const onChange = (event) => {
+    const { name, value, checked, type } = event.target;
+    const valueToUse =
+      type === "checkbox"
+        ? checked
+        : type === "number"
+        ? parseInt(value)
+        : value;
+    change(name, valueToUse);
+  };
+
+  return (
+    <form id="pizza-form" onSubmit={onSubmit}>
+      <h1>Build Your Own Pizza</h1>
+      <div>
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/52cf5852e4b0c1274ad7d85c/1427664439482-0HKP7K78UFF5YZD8M0B6/Uncle-Maddios-heart.jpg"
+          alt="A visual of a woman puttin sauce on pizza dough in the shape of a heart"
+        />
+      </div>
+      <div className="errors">
+        <div>{errors.name}</div>
+        <div>{errors.size}</div>
+        <div>{errors.sauce}</div>
+        <div>{errors.special}</div>
+        <div>{errors.quantity}</div>
+      </div>
+      <label>
+        Enter name for your order
+        <input
+          id="name-input"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={onChange}
+        />
+      </label>
+
+      <label>Customize Your Pizza</label>
+      <label>Choice of Size</label>
+      <select
+        id="size-dropdown"
+        name="size"
+        value={values.size}
+        onChange={onChange}
+      >
+        <option value="">-Select a size-</option>
+        <option value="Individual">Individual</option>
+        <option value="Small">Small</option>
+        <option value="Medium">Medium</option>
+        <option value="Large">Large</option>
+        <option value="X-Large">X-Large</option>
+      </select>
+
+      <label>Choice of Sauce</label>
+      <label>
+        <input
+          type="radio"
+          name="sauce"
+          value="originalRed"
+          onChange={onChange}
+          checked={values.sauce === "originalRed"}
+        />
+        Original Red
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sauce"
+          value="garlicRanch"
+          onChange={onChange}
+          checked={values.sauce === "garlicRanch"}
+        />
+        Garlic Ranch
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sauce"
+          value="bbq"
+          onChange={onChange}
+          checked={values.sauce === "bbq"}
+        />
+        BBQ Sauce
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sauce"
+          value="spinachAlfredo"
+          onChange={onChange}
+          checked={values.sauce === "spinachAlfredo"}
+        />
+        Spinach Alfredo
+      </label>
+      <label>Add Toppings</label>
+      <label>
+        <input
+          type="checkbox"
+          name="pepperoni"
+          checked={values.pepperoni}
+          onChange={onChange}
+        />
+        Pepperoni
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="sausage"
+          checked={values.sausage}
+          onChange={onChange}
+        />
+        Sausage
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="canadianBacon"
+          checked={values.canadianBacon}
+          onChange={onChange}
+        />
+        Canadian Bacon
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="spicyItalianSausage"
+          checked={values.spicyItalianSausage}
+          onChange={onChange}
+        />
+        Spicy Italian Sausage
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="grilledChicken"
+          checked={values.grilledChicken}
+          onChange={onChange}
+        />
+        Grilled Chicken
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="mushrooms"
+          checked={values.mushrooms}
+          onChange={onChange}
+        />
+        Mushrooms
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="bananaPeppers"
+          checked={values.bananaPeppers}
+          onChange={onChange}
+        />
+        Banana Peppers
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="onions"
+          checked={values.onions}
+          onChange={onChange}
+        />
+        Onions
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="greenPeppers"
+          checked={values.greenPeppers}
+          onChange={onChange}
+        />
+        Green Peppers
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="dicedTomatoes"
+          checked={values.dicedTomatoes}
+          onChange={onChange}
+        />
+        Diced Tomatoes
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="blackOlives"
+          checked={values.blackOlives}
+          onChange={onChange}
+        />
+        Black Olives
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="roastedGarlic"
+          checked={values.roastedGarlic}
+          onChange={onChange}
+        />
+        Roasted Garlic
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="artichokeHearts"
+          checked={values.artichokeHearts}
+          onChange={onChange}
+        />
+        Artichoke Hearts
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="threeCheese"
+          checked={values.threeCheese}
+          onChange={onChange}
+        />
+        Three Cheese
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="pineapple"
+          checked={values.pineapple}
+          onChange={onChange}
+        />
+        Pineapple
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          name="extraCheese"
+          checked={values.extraCheese}
+          onChange={onChange}
+        />
+        Extra Cheese
+      </label>
+      <label>Choice of Substitute</label>
+      <label>
+        <input
+          type="checkbox"
+          name="glutenFree"
+          checked={values.glutenFree}
+          onChange={onChange}
+        />
+        Gluten Free Crust (+ $1.00)
+      </label>
+      <label>
+        Special Instructions
+        <input
+          id="special-text"
+          value={values.special}
+          name="special"
+          type="text"
+          onChange={onChange}
+        />
+      </label>
+      <label>
+        Quantity:
+        <input
+          value={values.quantity}
+          name="quantity"
+          type="number"
+          onChange={onChange}
+        />
+      </label>
+      <button disabled={disabled} id="order-button" type="submit">
+        Add to Order
+      </button>
+    </form>
+  );
+}
